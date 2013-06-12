5e82132478b35c558689f5456f522fef219a25a6
Jeffrey Miller <jeffmax@gmail.com>
2013-06-12 10:55:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Django DICOM Study Review App
+
+# What is the Django DICOM Study Review App?
+This is a simple Django application to serve up DICOM studies for manual review before use within a research project. It should be used in conjunction with [StudyCentric](https://github.com/cbmi/studycentric) which will display the studies.
+
+# Installation
+To install, clone the repository and run 
+
+
+```pip install -U -r requirements.txt```
+
+This will install all dependencies.
+
+# Screenshot
+<center>
+<img src="https://raw.github.com/cbmi/django-dicom-review/master/dicom_review.png"/>
+</center>
---
+++ dicom_review/views.py
@@ -0,0 +1,51 @@
+import json
+from dicom_models.staging.models import RadiologyStudy
+from dicom_models.staging.models import RadiologyStudyReview as StudyReview
+from django.db import models
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.conf import settings
+from django.template import RequestContext
+from django.forms import ModelForm
+from django.views.decorators.cache import never_cache
+MAX_REVIEWERS = 1 # Maximum number of reviewers per study
+
+# Create the form class.
+class ReviewForm(ModelForm):
+    class Meta:
+        model = StudyReview
+
+@never_cache
+def review(request):
+    saved = 0 
+    if request.method == 'POST':
+        reviews = json.loads(request.raw_post_data)
+        for review in reviews:
+            review["user_id"] = request.user.id
+            form = ReviewForm(review)
+            if form.is_valid():
+                form.save()
+                saved+=1
+        return HttpResponse('{"saved":%d}' % saved, content_type = "application/json")
+    else:
+        return studies_page(request, saved)
+
+def studies_page(request, prev_saved):
+    studies = []
+    candidate_studies = RadiologyStudy.objects.filter(exclude=False, image_published=False, original_study_uid__isnull=False)
+    years = candidate_studies.dates('study_date', 'year')
+    for period in years:
+       this_year = candidate_studies.values('id').annotate(num_reviews=Count("radiologystudyreview"))\
+           .filter(study_date__year=period.year, num_reviews__lt=MAX_REVIEWERS)\
+           .exclude(radiologystudyreview__user_id=request.user.id).order_by("?")[:1]
+       for study in this_year:
+         studies.append(study['id'])
+
+    studies = RadiologyStudy.objects.filter(id__in=studies)
+
+    return render_to_response("index.html", {
+        'studies':studies, 'saved':prev_saved
+    }, RequestContext(request))
+
